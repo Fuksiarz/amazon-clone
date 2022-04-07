c5cfe7a65046961c2ee534eab4f9e3741e35a77f
Fuksiarz <adrianbloch8@gmail.com>
2022-04-07 22:24:48 +0200

    goinToSleep

---
+++ amazon-clone/src/Home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Home.css';
+function Home() {
+    return (
+        <div className="Home">
+            <div className='home__container'>
+                <img className="Home_image"
+                    src="https://m.media-amazon.com/images/I/6181GIcGcvL._SX3000_.jpg"
+                    alt=""
+                />
+            </div>
+
+        </div>
+    );
+}
+
+export default Home;
